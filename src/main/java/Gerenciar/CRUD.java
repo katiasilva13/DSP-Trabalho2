3c368c7c64e125bb7c7c7b22101a7f2b1bbbddfb
katiasilva13 <ktiax13@gmail.com>
2019-12-01 16:42:26 -0200

    CRUD: update (save/update dbBackup, update dbAtual)- v0.2.5

--- src/main/java/Gerenciar/CRUD.java
+++ src/main/java/Gerenciar/CRUD.java
@@ -15,6 +15,8 @@ import java.util.List;
  */
 public class CRUD {
 
+    String registro = "";
+
     public void save(boolean dbAtual, String tabela, String col1, String col2,
             String col3, String col4, String col5) {
 
@@ -74,94 +76,100 @@ public class CRUD {
 
     }
 
-    public static void getByID(boolean dbAtual, String tabela, long id) {
-        if (!dbAtual) {
-            DAOmysql daoMsql = new DAOmysql();
-            switch (tabela) {
-                case "Clientes":
-                    Clientes c;
-                    c = (Clientes) daoMsql.getByID(tabela, id);
-                    System.out.println("\nCliente: \nNome:" + c.getNome()
-                            + "\nSobrenome:" + c.getUltimoNome()
-                            + "\nEmail:" + c.getEmail()
-                            + "\nCelular:" + c.getCel()
-                            + "\nCPF:" + c.getCpf());
-                    break;
-                case "Funcionarios":
-                    Funcionarios f;
-                    f = (Funcionarios) daoMsql.getByID(tabela, id);
-                    System.out.println("\nFuncionario: \nNome:" + f.getNome()
-                            + "\nSobrenome:" + f.getUltimoNome()
-                            + "\nEmail:" + f.getEmail()
-                            + "\nCelular:" + f.getCel());
-                    break;
-                case "Produtos":
-                    Produtos p;
-                    p = (Produtos) daoMsql.getByID(tabela, id);
-                    System.out.println("\nProduto: \nNome:" + p.getNome()
-                            + "\nSobrenome:" + p.getFornecedor()
-                            + "\nEmail:" + p.getPreco()
-                            + "\nCelular:" + p.getCodigo());
-                    break;
-                case "Vendas":
-                    Vendas v;
-                    v = (Vendas) daoMsql.getByID(tabela, id);
-                    System.out.println("\nVenda: \nNome cliente:" + v.getNomeCliente()
-                            + "\nNome funcionario:" + v.getNomeFuncionario()
-                            + "\nNome produto:" + v.getNomeProduto()
-                            + "\nPreco produto:" + v.getPrecoProduto()
-                            + "\nCodigo produto:" + v.getCodigoProduto());
-                    break;
-                default:
-                    System.out.println("Erro");
-                    break;
+    public String getByID(boolean dbAtual, String tabela, long id) {
+        try {
+            if (!dbAtual) {
+                DAOmysql daoMsql = new DAOmysql();
+                switch (tabela) {
+                    case "Clientes":
+                        Clientes c;
+                        c = (Clientes) daoMsql.getByID(tabela, id);
+                        registro = "\nCliente: \nNome:" + c.getNome()
+                                + "\nSobrenome:" + c.getUltimoNome()
+                                + "\nEmail:" + c.getEmail()
+                                + "\nCelular:" + c.getCel()
+                                + "\nCPF:" + c.getCpf();
+                        break;
+                    case "Funcionarios":
+                        Funcionarios f;
+                        f = (Funcionarios) daoMsql.getByID(tabela, id);
+                        registro = "\nFuncionario: \nNome:" + f.getNome()
+                                + "\nSobrenome:" + f.getUltimoNome()
+                                + "\nEmail:" + f.getEmail()
+                                + "\nCelular:" + f.getCel();
+                        break;
+                    case "Produtos":
+                        Produtos p;
+                        p = (Produtos) daoMsql.getByID(tabela, id);
+                        registro = "\nProduto: \nNome:" + p.getNome()
+                                + "\nSobrenome:" + p.getFornecedor()
+                                + "\nEmail:" + p.getPreco()
+                                + "\nCelular:" + p.getCodigo();
+                        break;
+                    case "Vendas":
+                        Vendas v;
+                        v = (Vendas) daoMsql.getByID(tabela, id);
+                        registro = "\nVenda: \nNome cliente:" + v.getNomeCliente()
+                                + "\nNome funcionario:" + v.getNomeFuncionario()
+                                + "\nNome produto:" + v.getNomeProduto()
+                                + "\nPreco produto:" + v.getPrecoProduto()
+                                + "\nCodigo produto:" + v.getCodigoProduto();
+                        break;
+                    default:
+                        registro = "Ops! Algo deu errado.";
+                        break;
+                }
+            } else {
+                DAOpostgres daoPsql = new DAOpostgres();
+                switch (tabela) {
+                    case "Clientes":
+                        Clientes c;
+                        System.out.println("line124 CRUD");
+                        c = (Clientes) daoPsql.getByID(tabela, id);
+                        registro = "\nCliente: \nNome:" + c.getNome()
+                                + "\nSobrenome:" + c.getUltimoNome()
+                                + "\nEmail:" + c.getEmail()
+                                + "\nCelular:" + c.getCel() + "\nCPF:" + c.getCpf();
+                        break;
+
+                    case "Funcionarios":
+                        Funcionarios f;
+                        f = (Funcionarios) daoPsql.getByID(tabela, id);
+                        registro = "\nFuncionario: \nNome:" + f.getNome()
+                                + "\nSobrenome:" + f.getUltimoNome()
+                                + "\nEmail:" + f.getEmail()
+                                + "\nCelular:" + f.getCel();
+                        break;
+                    case "Produtos":
+                        Produtos p;
+                        p = (Produtos) daoPsql.getByID(tabela, id);
+                        registro = "\nProduto: \nNome:" + p.getNome()
+                                + "\nSobrenome:" + p.getFornecedor()
+                                + "\nEmail:" + p.getPreco()
+                                + "\nCelular:" + p.getCodigo();
+                        break;
+                    case "Vendas":
+                        Vendas v;
+                        v = (Vendas) daoPsql.getByID(tabela, id);
+                        registro = "\nVenda: \nNome cliente:" + v.getNomeCliente()
+                                + "\nNome funcionario:" + v.getNomeFuncionario()
+                                + "\nNome produto:" + v.getNomeProduto()
+                                + "\nPreco produto:" + v.getPrecoProduto()
+                                + "\nCodigo produto:" + v.getCodigoProduto();
+                        break;
+                    default:
+                        System.out.println("Ops! Algo deu errado.");
+                        break;
+                }
             }
-        } else {
-            DAOpostgres daoPsql = new DAOpostgres();
-            switch (tabela) {
-                case "Clientes":
-                    Clientes c;
-                    c = (Clientes) daoPsql.getByID(tabela, id);
-                    System.out.println("\nCliente: \nNome:" + c.getNome()
-                            + "\nSobrenome:" + c.getUltimoNome()
-                            + "\nEmail:" + c.getEmail()
-                            + "\nCelular:" + c.getCel()
-                            + "\nCPF:" + c.getCpf());
-                    break;
-                case "Funcionarios":
-                    Funcionarios f;
-                    f = (Funcionarios) daoPsql.getByID(tabela, id);
-                    System.out.println("\nFuncionario: \nNome:" + f.getNome()
-                            + "\nSobrenome:" + f.getUltimoNome()
-                            + "\nEmail:" + f.getEmail()
-                            + "\nCelular:" + f.getCel());
-                    break;
-                case "Produtos":
-                    Produtos p;
-                    p = (Produtos) daoPsql.getByID(tabela, id);
-                    System.out.println("\nProduto: \nNome:" + p.getNome()
-                            + "\nSobrenome:" + p.getFornecedor()
-                            + "\nEmail:" + p.getPreco()
-                            + "\nCelular:" + p.getCodigo());
-                    break;
-                case "Vendas":
-                    Vendas v;
-                    v = (Vendas) daoPsql.getByID(tabela, id);
-                    System.out.println("\nVenda: \nNome cliente:" + v.getNomeCliente()
-                            + "\nNome funcionario:" + v.getNomeFuncionario()
-                            + "\nNome produto:" + v.getNomeProduto()
-                            + "\nPreco produto:" + v.getPrecoProduto()
-                            + "\nCodigo produto:" + v.getCodigoProduto());
-                    break;
-                default:
-                    System.out.println("Erro");
-                    break;
-            }
+        } catch (Exception e) {
+            registro = "Ops! Algo deu errado.\nErro: " + e.getMessage();
         }
+        return registro;
 
     }
 
-    public static void getAllTable(boolean dbAtual, String tabela) {
+    public void getAllTable(boolean dbAtual, String tabela) {
         if (!dbAtual) {
             DAOmysql daoMsql = new DAOmysql();
             switch (tabela) {
@@ -250,4 +258,145 @@ public class CRUD {
 
     }
 
+    public boolean checkID(boolean dbAtual, String tabela, long id) {
+        boolean idCheck = false;
+        if (getByID(dbAtual, tabela, id).equalsIgnoreCase("Ops! Algo deu errado.\nErro: null")) {
+            idCheck = false;
+        } else if (getByID(dbAtual, tabela, id).equalsIgnoreCase("Ops! Algo deu errado.")) {
+            idCheck = false;
+        } else {
+            idCheck = true;
+        }
+        return idCheck;
+
+    }
+
+    public void update(boolean dbAtual, String tabela, long id, String col1, String col2,
+            String col3, String col4, String col5) {
+
+        CRUD crud = new CRUD();
+        if (!crud.checkID(dbAtual, tabela, id)) {
+            System.out.println("Ops! Algo deu errado.");
+        } else {
+            try {
+                if (!dbAtual) {
+                    DAOmysql daoMsql = new DAOmysql();
+                    switch (tabela) {
+                        //Cliente(nome, ultimoNome, email, cel, cpf);
+                        case "Clientes":
+                            Clientes c = (Clientes) daoMsql.getByID(tabela, id);                            
+                            c.setNome(col1);
+                            c.setUltimoNome(col2);
+                            c.setEmail(col3);
+                            c.setCel(col4);
+                            c.setCpf(col5);
+                            daoMsql.Update(c);
+                            break;
+                            //Funcionario(nome, ultimoNome, email, cel);    
+                        case "Funcionarios":
+                            Funcionarios f = (Funcionarios) daoMsql.getByID(tabela, id);
+                            f.setNome(col1);
+                            f.setUltimoNome(col2);
+                            f.setEmail(col3);
+                            f.setCel(col4);
+                            daoMsql.Update(f);
+                            break;
+                            //Produto(nome, fornecedor, preco, codigo);
+                        case "Produtos":
+                            Produtos p = (Produtos) daoMsql.getByID(tabela, id);
+                            p.setNome(col1);
+                            p.setFornecedor(col2);
+                            p.setPreco(col3);
+                            p.setCodigo(col4);
+                            daoMsql.Update(p);
+                            break;
+                            //Vendas(Produto.nome, Cliente.nome, Funcionário.nome, Produto.preco, Produto.codigo);    
+                        case "Vendas":
+                            Vendas v = (Vendas) daoMsql.getByID(tabela, id);
+                            v.setNomeProduto(col1);
+                            v.setNomeCliente(col2);
+                            v.setNomeFuncionario(col3);
+                            v.setPrecoProduto(col4);
+                            v.setCodigoProduto(col5);
+                            daoMsql.Update(v);
+                            break;
+                        default:
+                            System.out.println("Erro");
+                    }
+                }
+                DAOpostgres daoPsql = new DAOpostgres();
+                switch (tabela) {
+                    //Cliente(nome, ultimoNome, email, cel, cpf);
+                    case "Clientes":
+                        Clientes c = (Clientes) daoPsql.getByID(tabela, id);
+                        if(!checkID(false, tabela, id)){
+                            save(false, tabela, c.getNome(), c.getUltimoNome(), 
+                                    c.getEmail(), c.getCel(), c.getCpf());
+                        }else
+                            update(false, tabela, c.getId(),c.getNome(), 
+                                    c.getUltimoNome(), c.getEmail(), c.getCel(), c.getCpf());                    
+                        c.setNome(col1);
+                        c.setUltimoNome(col2);
+                        c.setEmail(col3);
+                        c.setCel(col4);
+                        c.setCpf(col5);
+                        daoPsql.Update(c);
+                        break;
+                        //Funcionario(nome, ultimoNome, email, cel);    
+                    case "Funcionarios":
+                        Funcionarios f = (Funcionarios) daoPsql.getByID(tabela, id);
+                        if(!checkID(false, tabela, id)){
+                            save(false, tabela, f.getNome(), f.getUltimoNome(),
+                                    f.getEmail(), f.getCel(),"");
+                        }else
+                            update(false, tabela, f.getId(),f.getNome(), f.getUltimoNome(),
+                                    f.getEmail(), f.getCel(),"");                          
+                        f.setNome(col1);
+                        f.setUltimoNome(col2);
+                        f.setEmail(col3);
+                        f.setCel(col4);
+                        daoPsql.Update(f);
+                        break;
+                        //Produto(nome, fornecedor, preco, codigo);
+                    case "Produtos":
+                        Produtos p = (Produtos) daoPsql.getByID(tabela, id);
+                        if(!checkID(false, tabela, id)){
+                            save(false, tabela, p.getNome(), p.getFornecedor(),
+                                    p.getPreco(), p.getCodigo(),"");
+                        }else
+                            update(false, tabela, p.getId(), p.getNome(), p.getFornecedor(),
+                                    p.getPreco(), p.getCodigo(),"");                         
+                        p.setNome(col1);
+                        p.setFornecedor(col2);
+                        p.setPreco(col3);
+                        p.setCodigo(col4);
+                        daoPsql.Update(p);
+                        break;
+                        //Vendas(Produto.nome, Cliente.nome, Funcionário.nome, Produto.preco, Produto.codigo);    
+                    case "Vendas":
+                        Vendas v = (Vendas) daoPsql.getByID(tabela, id);
+                        if(!checkID(false, tabela, id)){
+                            save(false, tabela, v.getNomeProduto(), v.getNomeCliente(), v.getNomeFuncionario(),
+                                v.getPrecoProduto(),v.getCodigoProduto());
+                        }else
+                            update(false, tabela, v.getId(),v.getNomeProduto(), v.getNomeCliente(), 
+                                    v.getNomeFuncionario(), v.getPrecoProduto(),v.getCodigoProduto());
+                        v.setNomeProduto(col1);
+                        v.setNomeCliente(col2);
+                        v.setNomeFuncionario(col3);
+                        v.setPrecoProduto(col4);
+                        v.setCodigoProduto(col5);
+                        daoPsql.Update(v);
+                        break;
+                    default:
+                        System.out.println("Erro");
+                }
+            } catch (Exception e) {
+                System.out.println("Ops! Algo deu errado.\nErro: " + e.getMessage());
+            }
+        }
+
+    }
+    
+    
 }
--- src/main/java/Menu/Menu.java
+++ src/main/java/Menu/Menu.java
@@ -26,27 +26,23 @@ public class Menu {
         //Cliente(nome, ultimoNome, email, cel, cpf);
         //Vendas(Produto.nome, Cliente.nome, Funcionário.nome, Produto.preco, Produto.codigo);   
         dbAtual = true;
-        id = 1;
-        tabela = "Funcionarios";
+        id = 3;
+        tabela = "Clientes";
         col1 = "";
         col2 = "";
         col3 = "";
         col4 = "";
         col5 = "";
         //  crud.save(dbAtual, tabela, col1, col2, col3, col4, col5);
-        //  crud.getByID(dbAtual, tabela, id);
-        crud.getAllTable(dbAtual, tabela);
-
+        //  System.out.println(crud.getByID(dbAtual, tabela, id) );
+        //  System.out.println("line 38 Menu, id true?"+crud.checkID(dbAtual, tabela, id));        
+        //crud.getAllTable(dbAtual, tabela);
+        crud.update(dbAtual, tabela, 1, "Daniela", "Estevão", "viviane@estevao.com", col4, col5);
+        
     }
     //TODO metodo update e delete em CRUD
 
-    private static void update(String tabela, long id, String nome) {
-        DAOmysql dao = new DAOmysql();
-        Entidades e = null;
-        e = dao.getByID(tabela, id);
-        e.setNome(nome);
-        dao.Update(e);
-    }
+    
 
     private static void delete(boolean dbAtual, String tabela, long id) {
         DAOmysql mysqlCon = new DAOmysql();
